68c9b098a691746b81928ddea6f8b0beb7616b1a
Aileen Wong <aileen.m.wong@gmail.com>
2017-10-01 16:30:13 -0400

    adds routes

--- planets/config/routes.rb
+++ planets/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'planets/random', to: 'planets#random'
+  resources :planets, only: [:index, :show]
+  resources :welcomes, only: [:index, :show]
+
+  root 'welcomes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
